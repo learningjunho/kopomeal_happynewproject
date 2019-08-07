e7afb15d1dc01e4f6cb33d5ea4bd395afe342584
서준호 <learningjunho@gmail.com>
2019-08-07 23:20:50 +0900

    boilerplate for crawling

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,10 @@
 from flask import render_template
 from app import app
+from app import crawl
+
+meal = crawl.get_meal()
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', meal = "학식")
+    return render_template('index.html', meal = meal)
