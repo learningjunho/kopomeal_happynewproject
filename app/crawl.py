05f8336d483490d9a616d4b9f8405ec6327cde3c
서준호 <learningjunho@gmail.com>
2019-08-08 22:44:11 +0900

    크롤링 관련 코드 추가

--- app/crawl.py
+++ app/crawl.py
@@ -1,2 +1,27 @@
+import requests
+
+meal = None
+lastcrawled = None
+
+def get_page(url):
+    return requests.get(url).text
+
+def parse_page(html):
+    table = html.find('<table class="tbl_table menu">')
+    tbody = html.find('<tbody>', table)
+
+    start = tbody
+    meal = []
+    for i in range(21):
+        start = html.find("<span>",start)
+        end = html.find("</span>",start)
+        meal.append(html[start+6:end])
+        start = end
+    return meal
+
 def get_meal():
-    return "boilerplate"
+    global meal
+    if meal == None:
+        html = get_page("http://www.kopo.ac.kr/anseong/content.do?menu=3295")
+        meal = parse_page(html)
+    return meal
