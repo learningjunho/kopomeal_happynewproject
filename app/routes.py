d177d4cf708d0d1891ef5edfe65a6bfd65236b17
서준호 <learningjunho@gmail.com>
2019-08-13 21:09:03 +0900

    show today's meal. 오늘의 학식 표시

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,13 @@
 from flask import render_template
 from app import app
 from app import crawl
+import datetime
 
 meal = crawl.get_meal()
+now = datetime.datetime.now()
+today_meal = meal[now.weekday()]
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', meal = meal)
+    return render_template('index.html', meal = meal, today_meal = today_meal)
