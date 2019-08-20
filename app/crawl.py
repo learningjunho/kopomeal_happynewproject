7704153c58319d8db8730ada4a6e3c7d319bca15
서준호 <learningjunho@gmail.com>
2019-08-20 17:11:07 +0900

    한 주가 지나면 자동으로 재크롤링 기능 추가

--- app/crawl.py
+++ app/crawl.py
@@ -1,6 +1,10 @@
 import requests
+import datetime
 
 meal = None
+lastcrawl = None
+nextcrawl = None
+
 
 def get_page(url):
     return requests.get(url).text
@@ -28,11 +32,23 @@ def parse_page(html):
             start = end
     return meal
 
+
+def get_expiration_date(date):
+    added_days = 7 - date.isoweekday()
+    d = datetime.timedelta(days = added_days)
+    return date + d
+
 def get_meal():
     global meal
-    if meal == None:
+    global lastcrawl
+    global nextcrawl
+
+
+    if meal == None or datetime.date.today() > nextcrawl:
         html = get_page("http://www.kopo.ac.kr/anseong/content.do?menu=3295")
         meal = parse_page(html)
+        lastcrawl = datetime.datetime.now()
+        nextcrawl = get_expiration_date(datetime.date.today())
     return meal
 
 # for debugging
--- app/routes.py
+++ app/routes.py
@@ -3,11 +3,13 @@ from app import app
 from app import crawl
 import datetime
 
-meal = crawl.get_meal()
+meal = None
 
 @app.route('/')
 @app.route('/index')
 def index():
+    global meal
+    meal = crawl.get_meal()
     now = datetime.datetime.now()
     today_meal = meal[now.weekday()]
     return render_template('index.html', meal = meal, today_meal = today_meal)
@@ -15,4 +17,4 @@ def index():
 @app.route('/debug')
 def debug():
     cached_meal = crawl.get_cached_meal()
-    return render_template("debug.html", meal = meal, cached_meal = cached_meal)
+    return render_template("debug.html", meal = meal, cached_meal = cached_meal, lastcrawl = crawl.lastcrawl, nextcrawl = crawl.nextcrawl)
