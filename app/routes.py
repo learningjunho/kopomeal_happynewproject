07dab47baa87b2e4e041b889691bd1b78a54daca
서준호 <learningjunho@gmail.com>
2019-08-20 15:23:13 +0900

    첫 실행후 요일이 갱신되지 않는 문제 수정

--- app/routes.py
+++ app/routes.py
@@ -4,15 +4,15 @@ from app import crawl
 import datetime
 
 meal = crawl.get_meal()
-now = datetime.datetime.now()
-today_meal = meal[now.weekday()]
 
 @app.route('/')
 @app.route('/index')
 def index():
+    now = datetime.datetime.now()
+    today_meal = meal[now.weekday()]
     return render_template('index.html', meal = meal, today_meal = today_meal)
 
 @app.route('/debug')
 def debug():
     cached_meal = crawl.get_cached_meal()
-    return render_template("debug.html", meal = meal, now = now, cached_meal = cached_meal)
+    return render_template("debug.html", meal = meal, cached_meal = cached_meal)
